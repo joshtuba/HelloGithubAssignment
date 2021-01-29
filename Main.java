12e11b822f90930388163857ace622dc2dd0118e
Josh <jaevans@gus.pittstate.edu>
2021-01-28 19:53:58 -0600

    now with the code!

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-//hello github
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Enter your name");
+    String name = keyboard.nextLine();
+    System.out.println("Hello " + name + ".");
+  }
+}
